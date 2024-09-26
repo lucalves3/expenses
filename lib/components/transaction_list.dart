49d476231efa9365413e89b4290d6e494c421e93
Lucas Alves <85763663+lucalves3@users.noreply.github.com>
2024-09-26 19:11:56 -0300

    add a image to project

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,55 +9,74 @@ class TransactionList extends StatelessWidget {
   TransactionList(this._transactions, this.onPressed);
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Container(
       height: 300,
       margin: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        itemCount: _transactions.length,
-        itemBuilder: (context, index) {
-          final tr = _transactions[index];
-          return Card(
-            child: Row(children: <Widget>[
+      child: _transactions.isEmpty
+          ? Column(children: <Widget>[
+              SizedBox(height: 20),
+              Text("Nenhuma transação cadastrada!",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 20),
               Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
+                height: 200,
+                child: Image.asset(
+                  "assets/images/waiting.png",
+                  fit: BoxFit.cover,
                 ),
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
-                    )),
-                padding: const EdgeInsets.all(10),
-                child: Text("R\$ ${tr.value.toStringAsFixed(2)}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.primary,
-                    )),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(tr.title,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      )),
-                  Text(DateFormat("d MMM y").format(tr.date),
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                      ))
-                ],
-              ),
-              IconButton(
-                  onPressed: () => onPressed(tr.id),
-                  icon: const Icon(Icons.delete)),
-            ]),
-          );
-        },
-      ),
+            ])
+          : ListView.builder(
+              itemCount: _transactions.length,
+              itemBuilder: (context, index) {
+                final tr = _transactions[index];
+                return Card(
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 2,
+                          )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text("R\$ ${tr.value.toStringAsFixed(2)}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).colorScheme.primary,
+                          )),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tr.title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Text(DateFormat("d MMM y").format(tr.date),
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                            ))
+                      ],
+                    ),
+                    IconButton(
+                        onPressed: () => onPressed(tr.id),
+                        icon: const Icon(Icons.delete)),
+                  ]),
+                );
+              },
+            ),
     );
   }
 }
