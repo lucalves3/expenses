ded669347146f8cb7c6544eb3302005d83e06a28
Lucas Alves <85763663+lucalves3@users.noreply.github.com>
2024-09-26 20:13:44 -0300

    learning about listview

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -35,45 +35,40 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (context, index) {
                 final tr = _transactions[index];
                 return Card(
-                  child: Row(children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(10)),
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2,
-                          )),
-                      padding: const EdgeInsets.all(10),
-                      child: Text("R\$ ${tr.value.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).colorScheme.primary,
-                          )),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tr.title,
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            'R\$${tr.value.toStringAsFixed(2)}',
                             style: const TextStyle(
-                              fontSize: 16,
+                              fontFamily: 'OpenSans',
                               fontWeight: FontWeight.bold,
-                            )),
-                        Text(DateFormat("d MMM y").format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey[600],
-                            ))
-                      ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    IconButton(
-                        onPressed: () => onPressed(tr.id),
-                        icon: const Icon(Icons.delete)),
-                  ]),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).colorScheme.primary,
+                      onPressed: () => onPressed(tr.id),
+                    ),
+                  ),
                 );
               },
             ),
